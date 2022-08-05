8ccd5634bd48828687d8ec610db21fbc3e7c3443
maintheme <oligarh20031@mail.ru>
2022-08-05 15:32:54 +0300

    108 out of 111 passed

--- python/4kyu/top3words.py
+++ python/4kyu/top3words.py
@@ -1,27 +1,31 @@
 def top_3_words(text):
     text = text.lower()
-    for ch in ['#','/', '\\', '.', '!', '@', '%', '^', '&', '*', '$', ',']:
+    for ch in ['#','/', '\\', '.', '!', '@', '%', '^', '&', '*', '$', ',', "'"]:
         if ch in text:
-            text = text.replace(ch, ' ')
+            if ch == "'":
+                if (text[text.find(ch)-1] == ' ' or text[text.find(ch)-1] == "'") and (text[text.find(ch)+1] == ' ' or text[text.find(ch)+1] == "'"):
+                    text = text.replace(ch, ' ')
+                else: continue      
+            else : text = text.replace(ch, ' ')
 
     words = text.split(' ')
 
     words_freq = {}
-
     for word in words:
         if word in words_freq: words_freq[word] += 1
         else: words_freq[word] = 1
 
-    words_freq = dict(sorted(words_freq.items(), reverse = True))
-
+    del words_freq['']
+  
+    words_freq = dict(sorted(words_freq.items(), key=lambda item: item[1], reverse=True))
+    
     top3 = []
-
-    for key in words_freq: 
-        while len(top3) != 3: 
-            top3.append(key)
+    for elem in words_freq:
+        while len(top3) != 3:
+            top3.append(elem)
             break
 
     return top3
 
 
-print(top_3_words("qwer$,qwer%qwe'r"))
+print(top_3_words(" qw ''  "))
